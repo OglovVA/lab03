d7b2c094cef30c8849a7c7972f9c6d0d4e67d893
Oglov Vlad <Vlad@example.com>
2020-04-17 10:43:03 +0400

    вызов гистограммы svg

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -93,6 +93,26 @@ void show_histogram_text(const vector<size_t> &bins)
     }
 }
 
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    svg_begin(400, 300);
+    svg_end();
+}
 
 
 int main()
@@ -108,6 +128,6 @@ int main()
     double min, max;
     find_minmax(numbers, min, max);
     const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }
