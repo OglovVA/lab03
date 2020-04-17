f865d22c50d0d9eb292154adec524f3503a22010
Oglov Vlad <Vlad@example.com>
2020-04-17 13:32:38 +0400

    histCPP

---
+++ lab03/histogram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "histogram.h"
+void find_minmax(const vector<double>& numbers, double& min, double& max)
+{
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers) {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
+    }
+}
