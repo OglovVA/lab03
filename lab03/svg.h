bc6a5b1f1c13bd663d18754358cce4b6459160cd
Oglov Vlad <Vlad@example.com>
2020-04-17 14:50:13 +0400

    svgH

--- lab03/svg.h
+++ lab03/svg.h
@@ -3,50 +3,20 @@
 
 #include <vector>
 using namespace std;
-void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black")
-{
-    cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
-}
+void svg_rect(double x, double y, double width, double height,string stroke,string fill );
+
 
 void
-svg_text(double left, double baseline, string text) {
-      cout << "<text x='" << left << "' y='" << baseline <<"'>" <<text <<"</text>";
-}
+svg_text(double left, double baseline, string text);
 
 void
-svg_begin(double width, double height) {
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
+svg_begin(double width, double height);
 
 void
-svg_end() {
-    cout << "</svg>\n";
-}
+svg_end();
 
 void
-show_histogram_svg(const vector<size_t>& bins) {
-const auto IMAGE_WIDTH = 400;
-const auto IMAGE_HEIGHT = 300;
-const auto TEXT_LEFT = 20;
-const auto TEXT_BASELINE = 20;
-const auto TEXT_WIDTH = 50;
-const auto BIN_HEIGHT = 30;
-const auto BLOCK_WIDTH = 10;
-double top = 0;
-svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-for (size_t bin : bins) {
-    const double bin_width = BLOCK_WIDTH * bin;
-    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"red","#aab5ff");
-    top += BIN_HEIGHT;
-}
-    svg_end();
-}
+show_histogram_svg(const vector<size_t>& bins);
 
 
 
