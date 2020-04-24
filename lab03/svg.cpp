44a0400b7c0a62e3f6dd850f95f081b73f04e4d1
Oglov Vlad <Vlad@example.com>
2020-04-24 22:20:32 +0400

    масштабирование работает

--- lab03/svg.cpp
+++ lab03/svg.cpp
@@ -30,18 +30,33 @@ svg_end() {
 void
 show_histogram_svg(const vector<size_t>& bins) {
 const auto IMAGE_WIDTH = 400;
+const size_t MAX_ASTERISK=35;
 const auto IMAGE_HEIGHT = 300;
 const auto TEXT_LEFT = 20;
 const auto TEXT_BASELINE = 20;
 const auto TEXT_WIDTH = 50;
 const auto BIN_HEIGHT = 30;
 const auto BLOCK_WIDTH = 10;
-double top = 0;
 svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-for (size_t bin : bins) {
-    const double bin_width = BLOCK_WIDTH * bin;
+double top = 0;
+unsigned max_count=0;
+for (size_t b : bins)
+{
+if(max_count<b)
+max_count=b;
+}
+for (size_t bin : bins)
+    {
+    size_t height=35;
+if(max_count>MAX_ASTERISK)
+height=MAX_ASTERISK*((static_cast<double>(bin))/max_count);
+else
+{
+height=bin;
+}
+height = BLOCK_WIDTH * height;
     svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"red","#aab5ff");
+    svg_rect(TEXT_WIDTH, top, height, BIN_HEIGHT,"red","#aab5ff");
 
     top += BIN_HEIGHT;
 }
