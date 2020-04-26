580a67849948c0605adeed14d4cf3e6f8e419e65
Oglov Vlad <Vlad@example.com>
2020-04-26 21:52:41 +0400

    final version 13 var

--- lab03/svg.cpp
+++ lab03/svg.cpp
@@ -39,7 +39,6 @@ for (size_t bin : bins)
 void
 show_histogram_svg(const vector<size_t>& bins) {
 const auto IMAGE_WIDTH = 400;
-const size_t MAX_ASTERISK=35;
 const auto TEXT_LEFT = 20;
 const auto TEXT_BASELINE = 20;
 const auto BIN_HEIGHT = 30;
@@ -50,26 +49,13 @@ size_t IMAGE_HEIGHT=(maximum*BLOCK_WIDTH)+30;
 double top = IMAGE_HEIGHT-20;
 svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 double TEXT_WIDTH=50;
-unsigned max_count=0;
-for (size_t b : bins)
-{
-if(max_count<b)
-max_count=b;
-}
 for (size_t bin : bins)
-    {
-    size_t height=35;
-if(max_count>MAX_ASTERISK)
-height=MAX_ASTERISK*((static_cast<double>(bin))/max_count);
-else
 {
-height=bin;
-}
-height = BLOCK_WIDTH * height;
 
+const double bin_width = BLOCK_WIDTH * bin;
 
 svg_text(TEXT_WIDTH+(BIN_HEIGHT/4.0), top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top-height, BIN_HEIGHT, height,"red","#aab5ff");
+    svg_rect(TEXT_WIDTH, top-bin_width, BIN_HEIGHT, bin_width ,"red","#aab5ff");
 TEXT_WIDTH+=BIN_HEIGHT;
 
 
