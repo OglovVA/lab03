b63ad64de2cc4004a265386bab2cda80c2f51416
Oglov Vlad <Vlad@example.com>
2020-04-29 12:12:13 +0400

    final

--- lab03/svg.h
+++ lab03/svg.h
@@ -1,6 +1,6 @@
 #ifndef SVG_H_INCLUDED
 #define SVG_H_INCLUDED
-
+#include <string>
 #include <vector>
 using namespace std;
 void svg_rect(double x, double y, double width, double height,string stroke,string fill );
@@ -14,6 +14,7 @@ svg_begin(double width, double height);
 
 void
 svg_end();
+void max_height(const vector<size_t>& bins,double& maximum);
 
 void
 show_histogram_svg(const vector<size_t>& bins);
