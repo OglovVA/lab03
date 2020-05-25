49cffe6ff93e8823e337bc836c684977d8027d6c
Oglov Vlad <Vlad@example.com>
2020-05-25 17:02:43 +0400

    3.1.1

--- lab03/main.cpp
+++ lab03/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 #include "svg.h"
 #include "histogram.h"
 
-vector<double> input_numbers(size_t count)
+vector<double> input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -89,7 +89,7 @@ int main()
     cerr << "Enter number count: ";
     cin >> number_count;
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin,number_count);
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
